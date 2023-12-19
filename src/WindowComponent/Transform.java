aadcb4a3ef0772265dac38d260f4541c2e6026ff
Fox Jensen <foxjensen@Foxs-MacBook-Air.local>
2023-12-19 10:53:14 -0800

    Adding the funny transforming java stuffies

---
+++ src/WindowComponent/Transform.java
@@ -0,0 +1,14 @@
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.awt.Image;
+import java.awt.image.RescaleOp;
+import javax.imageio.ImageIO;
+
+public class Transform {
+    public static BufferedImage zoom(BufferedImage buf, int x, int y, double percent) {
+        //returns a portion of an image zoomed with x, y as the center location
+    }
+    public static BufferedImage crop(BufferedImage buf, int xMin, int xMax, int yMin, int yMax) {
+        //crops to bounds
+    }
+}
