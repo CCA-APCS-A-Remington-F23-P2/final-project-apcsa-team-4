2eb249315f3a679865064826c3c2e778869b12ca
LMat-webdev <137461780+LMat-webdev@users.noreply.github.com>
2023-12-20 21:04:39 -0800

    Add files via upload

---
+++ src/WindowComponent/fluid.java
@@ -0,0 +1,14 @@
+package src.WindowComponent;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.awt.Image;
+import java.awt.image.RescaleOp;
+import javax.imageio.ImageIO;
+
+
+public class fluid {
+    public static BufferedImage crop() {
+        return null;
+    }
+}
